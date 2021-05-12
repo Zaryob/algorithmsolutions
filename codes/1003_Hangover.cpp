afd1e518a8b72899f1f690e5ecda1cd463fea261
Suleyman Poyraz <zaryob.dev@gmail.com>
2021-05-12 23:14:59 +0300

    Fixes made in codes.

--- codes/1003_Hangover.cpp
+++ codes/1003_Hangover.cpp
@@ -3,16 +3,16 @@
 using namespace std;
 
 int main() {
-	float a, len;
+  float a, len;
 
-	while(cin >> a, a) {
-		len = 0;
-		int i = 0;
-		while(len < a) {
-			len += 1.0 / (i + 2);
-			i ++;
-		}
-		cout << i << " card(s)" << endl;
-	}
-	return 0;
+  while(cin >> a, a) {
+    len = 0;
+    int i = 0;
+    while(len < a) {
+      len += 1.0 / (i + 2);
+      i ++;
+    }
+    cout << i << " card(s)" << endl;
+  }
+  return 0;
 }
